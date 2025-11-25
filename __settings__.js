05aeb303d70c522f93ae2afe387160f1c81e985c
Victor <victorvianeloc@gmail.com>
2025-11-24 22:32:27 -0300

    commit jogo PteroFlap

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2361953.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 263117117, 263926622, 263120555, 263119989, 263926621, 264810748, 263925330, 263117084, 263120778, 263120178, 263120054, 263926647, 263120013, 263117078, 263926623 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
